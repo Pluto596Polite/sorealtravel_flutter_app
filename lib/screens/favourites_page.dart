a6107abcdd390257d63c00945b26f0dfc30b4864
Pluto596Polite <avanwyk191@gmail.com>
2025-07-05 16:37:02 +0200

    Update FavouritePage layout with multiple containers and navigation button

--- lib/screens/favourites_page.dart
+++ lib/screens/favourites_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'home_page.dart';
 
 class FavouritePage extends StatelessWidget {
@@ -8,16 +9,88 @@ class FavouritePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Favourites')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-            );
-          },
-          child: const Text("Go Back to Login Page"),
-        ),
+      body: Column(
+        children: [
+          Container(
+            height: 130,
+            width: double.infinity,
+            color: Colors.blue,
+            child: Text("Container for Favourites Page"),
+            margin: const EdgeInsets.all(8.0),
+          ),
+          const Divider(
+            height: 2.5,
+            thickness: 2,
+            color: Colors.black,
+            indent: 10,
+            endIndent: 10,
+          ),
+
+          Container(
+            height: 130,
+            width: double.infinity,
+            color: Colors.green,
+            child: Text("Container for Favourites Page"),
+            margin: const EdgeInsets.all(8.0),
+          ),
+
+          const Divider(
+            height: 2.5,
+            thickness: 2,
+            color: Colors.black,
+            indent: 10,
+            endIndent: 10,
+          ),
+
+          Container(
+            height: 130,
+            width: double.infinity,
+            color: Colors.red,
+            child: Text("Container for Favourites Page"),
+            margin: const EdgeInsets.all(8.0),
+          ),
+          const Divider(
+            height: 2.5,
+            thickness: 2,
+            color: Colors.black,
+            indent: 10,
+            endIndent: 10,
+          ),
+
+          Container(
+            height: 130,
+            width: double.infinity,
+            color: Colors.yellow,
+            child: Text("Container for Favourites Page"),
+            margin: const EdgeInsets.all(8.0),
+          ),
+
+          const Divider(
+            height: 2.5,
+            thickness: 2,
+            color: Colors.black,
+            indent: 10,
+            endIndent: 10,
+          ),
+          
+          Container(
+            height: 130,
+            width: double.infinity,
+            color: Colors.purple,
+            child: Text("Container for Favourites Page"),
+            margin: const EdgeInsets.all(8.0),
+          ),
+
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            },
+            child: const Text("Go Back to Login Page"),
+          ),
+        ],
       ),
     );
   }
